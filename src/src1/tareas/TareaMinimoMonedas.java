55f16e2cf30554d50b3ad02deafc9543901bb39b
Raul Reza <raulreza@hotmail.com>
2018-07-06 19:59:23 -0600

    Completed Homework

--- src/src1/tareas/TareaMinimoMonedas.java
+++ src/src1/tareas/TareaMinimoMonedas.java
@@ -4,31 +4,31 @@ import libs.Input;
 
 public class TareaMinimoMonedas {
 
-static int numMonedas;
-static int remanente;
+	static int numMonedas = 0;
+	static int remanente;
 
-	public static void main(String[] args) {
-		float tot = preguntamonto();
-		calc25(tot);
-		//calc10();
-		//calc5():
-		//calc1();
-		Input.print(numMonedas);
+		public static void main(String[] args) {
+			int remanente = preguntaMonto();
+			remanente = calc(remanente, 25);
+			remanente = calc(remanente, 10);
+			remanente = calc(remanente, 5);
+			calc(remanente, 1);
+			Input.print(numMonedas);
+		}
+
+		private static int preguntaMonto() {
+			Input.print("Cuánto cambio te debo? ");
+			double x = Input.get_double();
+			int tot = (int) (x * 100);
+			return tot;
+		}
 		
-		
-	}
-
-	private static void calc25(int m25) {
-		m25 = (m25 / 25);
-		System.out.print(m25 + " monedas de 25");
-		return numMonedas;
-		return remanente;
-	}
-
-	private static float preguntamonto() {
-		System.out.print("Cuánto cambio te debo? ");
-		float tot = Input.get_float();
-		return tot;
-	}
-
+		private static int calc(int tot, int i) {
+			int div = (tot/i);
+			int sob = (tot % i);
+			if (div > 0) {
+				numMonedas = numMonedas + div;
+			}
+			return remanente = sob;
+		}
 }
